8daa8699555c3ff498675c873a2ed8f6946e43b2
klequis <klequis@gmail.com>
2024-05-18 16:28:51 -0700

    u

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,11 +1,8 @@
-import { HttpStatusCode } from "@solidjs/start";
-
 export default function NotFound() {
   return (
-    <main class="component">
-      <HttpStatusCode code={404} />
+    <div class="component">
       <p class="file-name">routes/[...404].tsx</p>
       <h1>Page Not Found</h1>
-    </main>
+    </div>
   );
 }
--- src/routes/contacts.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contacts() {
-  return (
-    <div class="component">
-      <p class="file-name">routes/contacts.jsx</p>
-    </div>
-  );
-}
