fbc529b8820b9b0cc200771b6431eef467a76c3a
klequis <klequis@gmail.com>
2024-05-18 16:45:18 -0700

    u

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,8 @@ export default function App() {
     <Router
       root={(props) => (
         <div class="component background-purple">
-          <p class="file-name">app.tsx</p>
+          <div class="file-name">app.tsx</div>
+          <div class="my-background">My background is purple.</div>
           <a class="button-1" href="/">/index</a>
           <a class="button-1" href="/about">/about</a>
           <Suspense>{props.children}</Suspense>
